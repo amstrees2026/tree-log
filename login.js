dd5433e9fea7a8cf8ff19662bf886f247b2778e7
amstrees2026 <trees.ams2026@gmail.com>
2026-07-30 13:22:24 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,16 +1,29 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
+import {
+    getAuth,
+    signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+
+// Firebase Config
 const firebaseConfig = {
+
     apiKey: "AIzaSyD2XTkILn4iMgR_LE6sDlqI_08NaTV5ebo",
+
     authDomain: "tree-log-42cf3.firebaseapp.com",
+
     projectId: "tree-log-42cf3",
+
     storageBucket: "tree-log-42cf3.firebasestorage.app",
+
     messagingSenderId: "580957773231",
+
     appId: "1:580957773231:web:d9a8301c3a869e157fa35a"
+
 };
 
 const app = initializeApp(firebaseConfig);
+
 const auth = getAuth(app);
 
 document.getElementById("loginBtn").addEventListener("click", login);
@@ -18,23 +31,30 @@ document.getElementById("loginBtn").addEventListener("click", login);
 async function login(){
 
     const email = document.getElementById("email").value.trim();
+
     const password = document.getElementById("password").value;
 
     try{
 
-        await signInWithEmailAndPassword(auth, email, password);
+        await signInWithEmailAndPassword(auth,email,password);
 
-        document.getElementById("status").style.color = "green";
-        document.getElementById("status").innerHTML = "✅ Login Successful!";
+        document.getElementById("status").style.color="green";
+
+        document.getElementById("status").innerHTML="✅ Login Successful";
 
         setTimeout(function(){
-            window.location.href = "admin.html";
-        },800);
 
-    }catch(error){
+            window.location.href="admin.html";
 
-        document.getElementById("status").style.color = "red";
-        document.getElementById("status").innerHTML = error.message;
+        },1000);
+
+    }
+
+    catch(error){
+
+        document.getElementById("status").style.color="red";
+
+        document.getElementById("status").innerHTML=error.message;
 
     }
 
